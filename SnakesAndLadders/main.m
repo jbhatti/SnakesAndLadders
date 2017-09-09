21a258bc88d91d26f5e51be75d921c9d102df094
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-09 18:26:30 -0400

    first commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Jaison Bhatti on 2017-09-09.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
