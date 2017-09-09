2677bb3ef1ce57415c47c7d7486a463fc2a5277e
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-09 18:56:14 -0400

    added player class

---
+++ SnakesAndLadders/Player.h
@@ -0,0 +1,17 @@
+//
+//  Player.h
+//  SnakesAndLadders
+//
+//  Created by Jaison Bhatti on 2017-09-09.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@property NSInteger currentSquare;
+
+-(void) roll;
+
+@end
---
+++ SnakesAndLadders/Player.m
@@ -0,0 +1,16 @@
+//
+//  Player.m
+//  SnakesAndLadders
+//
+//  Created by Jaison Bhatti on 2017-09-09.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+
+}
+
+@end
